022956e5242ed52f187fe8f3df6e52de692fe4b8
Aleksandr Romanov <>
2024-11-17 10:21:36 +0300

    Правки 2

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,9 +1,8 @@
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
-  links: [
-  ],
-  actions: [{ text: 'Download', href: 'https://apple.co/3bcPIIS', target: '_blank' }],
+  links: [],
+  actions: [],
 };
 
 export const footerData = {
@@ -15,7 +14,7 @@ export const footerData = {
     },
     {
       links: [
-       { text: 'Support', href: '#' }
+       { text: 'Support', href: 'mailto:support@budgethound.app' }
        ]
     },
     {
@@ -25,12 +24,6 @@ export const footerData = {
       ]
     }
   ],
-  secondaryLinks: [
-  ],
-  socialLinks: [
-  ],
-  footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  `,
+  secondaryLinks: [],
+  socialLinks: []
 };
